a948488e4c04e1362483eda945c009bb7882c234
Vaibhav Khandekar <32119720+vaibhau@users.noreply.github.com>
2023-07-13 12:53:40 +0530

    Create PalindromeNumber.py

---
+++ Python/PalindromeNumber.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        x = str(x)
+        n = x[::-1]
+        if x == n:
+            return True
+        else:
+            return False
