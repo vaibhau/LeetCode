a0e925f77e6911473f16dc023b346e44e96736fd
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2023-10-16 12:30:21 +0530

    Updated 50. Pow(x,n).py

--- Python/50. Pow(x,n).py
+++ Python/50. Pow(x,n).py
@@ -3,3 +3,9 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         return x**n
+# Solution 2
+
+import math
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return math.pow(x,n)
