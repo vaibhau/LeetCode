9c5fb6f217b20ac7937bce1e78c0f957666ec0b4
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2024-02-12 16:28:18 +0530

    Update 34. Find First and Last Position of Element in Sorted Array.java

--- Java/34. Find First and Last Position of Element in Sorted Array.java
+++ Java/34. Find First and Last Position of Element in Sorted Array.java
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 
-// Solution 1
+// Solution 1 - brut force
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         if (nums.length == 0) {
@@ -21,5 +21,56 @@ class Solution {
             }
         }
         return new int[]{i1,i2};
+    }
+}
+
+// Solution 2 - 
+
+package LeetCode;
+
+import java.util.Arrays;
+
+public class FirstLastIndex {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,3,3,3,4,4,4,4,5,5,5,5,5};
+        int target = 4;
+        int[] ans = searchRange(arr,target);
+        System.out.print(Arrays.toString(ans));
+    }
+
+    static int[] searchRange(int[] arr, int target) {
+        int[] ans = {-1,-1};
+
+        ans[0] = search(arr, target, true);
+        if (ans[0] != -1) {
+            ans[1] = search(arr, target, false);
+        }
+
+        return ans;
+    }
+
+    static int search(int[] arr, int target, boolean startIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = arr.length - 1 ;
+        while (start <= end) {
+            int middle = (start + end) / 2; // start + (end - start) / 2
+
+            if (target < arr[middle]) {
+                end = middle - 1;
+            } else if (target > arr[middle]) {
+                start = middle + 1;
+            } else {
+                // potential answer found
+                ans = middle;
+
+                if(startIndex) {
+                    end = middle - 1;
+                } else {
+                    start = middle + 1;
+                }
+            }
+        }
+        return ans;
     }
 }
