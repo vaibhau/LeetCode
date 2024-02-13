54a33ffa87db1dfc7e6db69570d267ead7139fad
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2024-02-13 17:44:24 +0530

    Create 162. Find Peak Element.java

---
+++ Java/162. Find Peak Element.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/find-peak-element/description/
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start < end) {
+            int middle = start + (end - start) / 2;
+            if (nums[middle] > nums[middle + 1]) {
+                end = middle;
+            } else {
+                start = middle + 1;
+            }
+        }
+        return start;
+    }
+}
