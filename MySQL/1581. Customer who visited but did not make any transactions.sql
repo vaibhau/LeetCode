ae224a64a3e178af31bac12629b4263f3fb2f5ad
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2024-03-06 13:23:32 +0530

    Create 1581. Customer who visited but did not make any transactions.sql

---
+++ MySQL/1581. Customer who visited but did not make any transactions.sql
@@ -0,0 +1,8 @@
+# Solution 1
+
+SELECT v.customer_id, COUNT(*) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY customer_id;
